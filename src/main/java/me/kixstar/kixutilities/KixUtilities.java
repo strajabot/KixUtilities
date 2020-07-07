8c1af2ef7c23f707a19c3ace4d3521eeacca8e67
strajabot <Strajabot@gmail.com>
2020-07-07 13:17:08 +0200

    Add packets to support nickname management inside a Bungeecord plugin

--- src/main/java/me/kixstar/kixutilities/KixUtilities.java
+++ src/main/java/me/kixstar/kixutilities/KixUtilities.java
@@ -1,13 +1,20 @@
 package me.kixstar.kixutilities;
 
+import me.kixstar.kixutilities.feature.nickname.BungeeNicknameSync;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class KixUtilities extends JavaPlugin {
     public void onEnable() {
+
+        BungeeNicknameSync bungeeNicknameSync = new BungeeNicknameSync();
+        getServer().getMessenger().registerIncomingPluginChannel(this, "kixutilities:nickname", bungeeNicknameSync);
+        getServer().getPluginManager().registerEvents(bungeeNicknameSync,this);
+
     }
 
     public void onDisable() {
+        getServer().getMessenger().unregisterIncomingPluginChannel(this);
     }
 
     public static KixUtilities getInstance() {
---
+++ src/main/java/me/kixstar/kixutilities/KixstarDB.java
@@ -0,0 +1,11 @@
+package me.kixstar.kixutilities;
+
+import org.bukkit.entity.Player;
+
+public class KixstarDB {
+
+    public static String getNickname(Player player) {
+        //todo: implement database
+        return null;
+    }
+}
